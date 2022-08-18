a6dda09ac9a19c8698af0da01d254eebfb9ac155
Chani <lsc90726@gmail.com>
2022-08-18 18:26:51 +0900

    2. Creating Header

---
+++ src/header/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./header.css";
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="headerTitles">
+        <span className="headerTitleSm">CHAN</span>
+        <span className="headerTitleLg">PLAYGROUND</span>
+      </div>
+      <img
+        className="headerImg"
+        src="https://images.unsplash.com/photo-1488998427799-e3362cec87c3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+        alt="main"
+      />
+    </div>
+  );
+};
+
+export default Header;
