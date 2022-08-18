c02cc1af20b4554e69c542ee9ee26f8318589bcf
Chani <lsc90726@gmail.com>
2022-08-19 01:57:29 +0900

    6. Creating Write

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import Topbar from "./components/topbar/Topbar";
 import Header from "./components/header/Header";
 import Home from "./pages/home/Home";
 import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
 
 function App() {
   return (
     <div className="App">
       <Topbar />
-      <Single />
+      <Write />
     </div>
   );
 }
