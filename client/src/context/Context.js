7db7978da6446d3a043ba31512e2362f16bd0eb9
Chani <lsc90726@gmail.com>
2022-08-29 17:07:40 +0900

    16. Setting Context

---
+++ client/src/context/Context.js
@@ -0,0 +1,28 @@
+import { createContext, useReducer } from "react";
+import Reducer from "./Reducer";
+
+//초기값 지정
+const INITIAL_STATE = {
+  user: null,
+  isFetching: false,
+  error: false,
+};
+
+export const Context = createContext();
+
+export const ContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(Reducer, INITIAL_STATE);
+
+  return (
+    <Context.Provider
+      value={{
+        user: state.user,
+        isFetching: state.isFetching,
+        error: state.error,
+        dispatch,
+      }}
+    >
+      {children}
+    </Context.Provider>
+  );
+};
